c3a1a166ffdc39fed7b4ec8c9c85505d1526ee7e
hangnadi <hangnadi@gmail.com>
2017-05-11 14:02:09 +0700

    Collapse Bar 3 - extract class from MainActivity.class

--- CollapseBar3/src/main/java/com/hangnadi/toolbar/collapse/MainActivity.java
+++ CollapseBar3/src/main/java/com/hangnadi/toolbar/collapse/MainActivity.java
@@ -1,22 +1,18 @@
 package com.hangnadi.toolbar.collapse;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -53,6 +49,32 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
+        loadImage();
+    }
+
+    private void loadImage() {
+        ImageView bgHeader = (ImageView) findViewById(R.id.bgheader);
+        Glide.with(this)
+                .load(getRandomBgImage())
+                .centerCrop()
+                .into(bgHeader);
+    }
+
+    private String getRandomBgImage() {
+        Random random = new Random();
+        switch (random.nextInt(5)) {
+            default:
+            case 0:
+                return "https://github.com/chrisbanes/cheesesquare/blob/master/app/src/main/res/drawable-nodpi/cheese_1.jpg?raw=true";
+            case 1:
+                return "https://github.com/chrisbanes/cheesesquare/blob/master/app/src/main/res/drawable-nodpi/cheese_2.jpg?raw=true";
+            case 2:
+                return "https://github.com/chrisbanes/cheesesquare/blob/master/app/src/main/res/drawable-nodpi/cheese_3.jpg?raw=true";
+            case 3:
+                return "https://github.com/chrisbanes/cheesesquare/blob/master/app/src/main/res/drawable-nodpi/cheese_4.jpg?raw=true";
+            case 4:
+                return "https://github.com/chrisbanes/cheesesquare/blob/master/app/src/main/res/drawable-nodpi/cheese_5.jpg?raw=true";
+        }
     }
 
     @Override
@@ -76,79 +98,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
-
-        @Override
-        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-//            TextView textView = (TextView) view.findViewById(R.id.section_label);
-//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
 }
